8720ef8da2321530f24557a1096b2b70e2ee7b5c
rshahoud2-png <rshahoud2@gmail.com>
2026-06-12 11:53:22 -0400

    Add Electron main process

---
+++ electron/main.cjs
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 940,
+    minWidth: 1180,
+    minHeight: 760,
+    backgroundColor: '#06080d',
+    title: 'AI Penny Stock Terminal',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
